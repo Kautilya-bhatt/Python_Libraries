0d63799c5c83402080a245b5d5018c730a4b2cfc
kautilya-bhatt <kautilyabhatt123@gmail.com>
2026-08-30 19:58:00 +0530

    Strong Password generator push

---
+++ strong_password_generator.py
@@ -0,0 +1,11 @@
+import random
+lower ="abcdefghjklmnopqrstuvwxyz"
+upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number ="1234567890"
+symbols ="@#$&"
+all =lower+upper+number+symbols
+
+length = 12
+
+password="".join(random.sample(all,length))
+print(password)
